fb9d27cdf2553dc096c1db05c253bc1ae0cb1c56
Mangloo69 <yadav69.swayam@gmail.com>
2025-08-20 19:30:50 +0545

    Database design and role based route setup with proper icons and tags

--- components/header.jsx
+++ components/header.jsx
@@ -2,15 +2,22 @@ import {
   SignedIn,
   SignedOut,
   SignInButton,
-  SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { checkUser } from "@/lib/checkUser";
+import {
+  CalendarClock,
+  ShieldUser,
+  Stethoscope,
+  User,
+} from "lucide-react";
 
-const Header = () => {
+const Header = async () => {
+  const user = await checkUser();
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4  h-16 flex items-center justify-between">
@@ -25,15 +32,89 @@ const Header = () => {
         </Link>
 
         <div className="flex items-center space-x-2">
+          <SignedIn>
+            {user?.role === "ADMIN" && (
+              <Link href={"/admin"}>
+                <Button
+                  variant={"outline"}
+                  className={
+                    "hidden md:inline-flex items-center gap-2"
+                  }>
+                  <ShieldUser className="h-4 w-4" />
+                  Admin Dashboard
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  className={"md:hidden h-10 w-10 p-0"}>
+                  <ShieldUser className="h-4 w-4" />
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+          <SignedIn>
+            {user?.role === "DOCTOR" && (
+              <Link href={"/doctor"}>
+                <Button
+                  variant={"outline"}
+                  className={
+                    "hidden md:inline-flex items-center gap-2"
+                  }>
+                  <Stethoscope className="h-4 w-4" />
+                  Doctor Dashboard
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  className={"md:hidden h-10 w-10 p-0"}>
+                  <Stethoscope className="h-4 w-4" />
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+
+          <SignedIn>
+            {user?.role === "PATIENT" && (
+              <Link href={"/appointments"}>
+                <Button
+                  variant={"outline"}
+                  className={
+                    "hidden md:inline-flex items-center gap-2"
+                  }>
+                  <CalendarClock className="h-4 w-4" />
+                  My Appointments
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  className={"md:hidden h-10 w-10 p-0"}>
+                  <CalendarClock className="h-4 w-4" />
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+
+          <SignedIn>
+            {user?.role === "UNASSIGNED" && (
+              <Link href={"/onboarding"}>
+                <Button
+                  variant={"outline"}
+                  className={
+                    "hidden md:inline-flex items-center gap-2"
+                  }>
+                  <User className="h-4 w-4" />
+                  Complete Profile
+                </Button>
+                <Button
+                  variant={"ghost"}
+                  className={"md:hidden h-10 w-10 p-0"}>
+                  <User className="h-4 w-4" />
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+
           <SignedOut>
             <SignInButton>
               <Button variant="secondary">Sign In</Button>
             </SignInButton>
-            {/* <SignUpButton>
-              <Button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </Button>
-            </SignUpButton> */}
           </SignedOut>
           <SignedIn>
             <UserButton
